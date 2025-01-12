ef91d5bfcfcdcaedc7169a7a39e3c6fede972060
SARAVANAN <144509113+SARAVANAN15032003@users.noreply.github.com>
2025-01-12 09:18:01 +0530

    Create Sort 0s, 1s and 2s.java

---
+++ Sort 0s, 1s and 2s.java
@@ -0,0 +1,19 @@
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+       /* for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]>a[j]){
+                int t=a[i];
+                a[i]=a[j];
+                a[j]=t;}
+            }
+        }*/
+        Arrays.sort(a);
+       
+    }
+}
